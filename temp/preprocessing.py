e7a0b2dabc890fd584f53fbcc16beda53fb28b2c
czuluagaz <139249956+czuluagaz@users.noreply.github.com>
2024-02-04 12:19:25 +0100

    not going anywhere

--- temp/preprocessing.py
+++ temp/preprocessing.py
@@ -15,9 +15,39 @@ dataset = 'data_storage/raw_data.csv'
 # Read the csv file
 df = pd.read_csv(dataset, decimal=',') # read the csv file and change ',' to '.'
 
-# change name of 6 and 7 columns
-df.rename(columns={'Unnamed: 6': 'consumption', 'Unnamed: 7': 'cost'}, inplace=True)
+# drop columns 6 and 7
+df.drop(df.columns[[6, 7]], axis=1, inplace=True)
 
+
+# format 'date' column to datetime
+df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
+
+
+df.describe()
+print(df.info()) 
+
+
+
+# # in the column 'date' add the lines for the missing months from 31/01/2011 to 31/01/2024
+# df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
+# df = df.set_index('date').resample('M').asfreq().reset_index()
+
+# # fill the missing values with the previous value
+# df = df.fillna(method='ffill')
+
+
+# # create a column with (chrono_date) that contains the dates starting 31/01/2019 to 31/12/202024
+# df['chrono_date'] = pd.date_range(start='31/01/2019', end='31/12/2024', freq='Month')
+
+# # create 3 new dataframes (water, power, gas), define the columns for each dataframe and save them as csv files
+# df_water = df[['Date', 'Time', 'Water']]
+# df_water.to_csv('data_storage/water.csv', index=False)
+
+# df_power = df[['Date', 'Time', 'Power']]
+# df_power.to_csv('data_storage/power.csv', index=False)
+
+# df_gas = df[['Date', 'Time', 'Gas']]
+# df_gas.to_csv('data_storage/gas.csv', index=False)
 
 
 print(df.head(5))
