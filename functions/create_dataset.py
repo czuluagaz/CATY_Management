4f90d1c496baa91e9733765e10f8bc9c83623cfe
czuluagaz <139249956+czuluagaz@users.noreply.github.com>
2024-02-04 23:24:05 +0100

    updated version create dataset

--- functions/create_dataset.py
+++ functions/create_dataset.py
@@ -15,29 +15,31 @@ start = time.time()
 dataset = "data_storage/raw_data.csv"
 
 # Read the csv file
-df = pd.read_csv(dataset, decimal=",")  # read the csv file and change ',' to '.'
+df = pd.read_csv(dataset, sep=";")
 
 # drop columns 6 and 7
 df.drop(df.columns[[1, 6, 7]], axis=1, inplace=True)
 
-# split column 'date' into 3 columns 'day', 'month', 'year'
-df[["day", "month", "year"]] = df["date"].str.split("/", expand=True)
-
-# in column 'year' if year is YY change it to YYYY
-df["year"] = df["year"].apply(lambda x: "20" + x if len(x) == 2 else x)
-
-# merge columns 'day', 'month', 'year' into a new column 'date' format YYYY-MM-DD
-df["date"] = pd.to_datetime(df[["year", "month", "day"]])
-df = df.drop(columns=["day", "month", "year"])
+# convert date to datetime
+df["date"] = pd.to_datetime(df["date"], format="%m/%d/%Y")
 
 # create a column "date_eom" with the end of the month from the column 'date'
 df["date_eom"] = pd.to_datetime(df["date"], format="dd-mm-YYYY") + pd.offsets.MonthEnd(
     0
 )
 
-df.info()
 # in headers change ' ' to '_'
 df.columns = df.columns.str.replace(" ", "_")
+
+# rename columns 7, 8, 11 as 'cons', 'av_day.1', 'av_day.2' respectively
+df.rename(
+    columns={
+        df.columns[6]: "cons",
+        df.columns[7]: "av_day.1",
+        df.columns[10]: "av_day.2",
+    },
+    inplace=True,
+)
 
 # create a new df (dfref) with a column that contains dates from 31/01/2011 to 31/12/2024
 dfref = pd.DataFrame(
@@ -46,8 +48,6 @@ dfref = pd.DataFrame(
 
 # merge df and dfref in date_eom
 df = pd.merge(dfref, df, on="date_eom", how="left")
-
-print(df.head(100))
 
 # generate 3 csv files (water, power, gas) with the columns
 df_water = df[
