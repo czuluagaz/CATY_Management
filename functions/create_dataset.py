1d4feb5a78a6762fadb78348142a4827b6c75f8a
czuluagaz <139249956+czuluagaz@users.noreply.github.com>
2024-06-08 15:43:46 +0200

    cleanning and return to basic

--- functions/create_dataset.py
+++ functions/create_dataset.py
@@ -4,8 +4,9 @@
 # Date: 2023-12-01
 
 # Importing required libraries
-import pandas as pd
 import time
+import pandas as pd
+
 
 # Initialize the timer
 start = time.time()
@@ -15,31 +16,27 @@ start = time.time()
 dataset = "data_storage/raw_data.csv"
 
 # Read the csv file
-df = pd.read_csv(dataset, sep=";", decimal=",")
+df = pd.read_csv(dataset, sep=",", encoding="utf-8", decimal=".")
 
-# drop columns 6 and 7
-df.drop(df.columns[[1, 6, 7]], axis=1, inplace=True)
+# # drop columns 6 and 7
+# df.drop(df.columns[[1, 6, 7]], axis=1, inplace=True)
 
 # convert date to datetime
-df["date"] = pd.to_datetime(df["date"], format="%m/%d/%Y")
-
-# create a column "date_eom" with the end of the month from the column 'date'
-df["date_eom"] = pd.to_datetime(df["date"], format="dd-mm-YYYY") + pd.offsets.MonthEnd(
-    0
-)
+df["date"] = pd.to_datetime(df["date"], format="%d/%m/%Y")
+df["date_eom"] = df["date"] + pd.offsets.MonthEnd(1)
 
 # in headers change ' ' to '_'
 df.columns = df.columns.str.replace(" ", "_")
 
-# rename columns 7, 8, 11 as 'cons', 'av_day.1', 'av_day.2' respectively
-df.rename(
-    columns={
-        df.columns[6]: "cons",
-        df.columns[7]: "av_day.1",
-        df.columns[10]: "av_day.2",
-    },
-    inplace=True,
-)
+# # rename columns 7, 8, 11 as 'cons', 'av_day.1', 'av_day.2' respectively
+# df.rename(
+#     columns={
+#         df.columns[6]: "cons",
+#         df.columns[7]: "av_day.1",
+#         df.columns[10]: "av_day.2",
+#     },
+#     inplace=True,
+# )
 
 # create a new df (dfref) with a column that contains dates
 # from 31/01/2011 to 31/12/2024
@@ -57,8 +54,8 @@ df_water = df[
         "date",
         "days",
         "water_m3",
-        "cons",
-        "av_day.1",
+        "w_cons",
+        "w_av_day",
     ]
 ]
 df_water.to_csv("data_storage/water.csv", index=False)
@@ -68,9 +65,9 @@ df_power = df[
         "date_eom",
         "date",
         "days",
-        "power_kw/h",
-        "cons_power",
-        "av_day.2",
+        "power_kwh",
+        "p_cons",
+        "p_av_day",
     ]
 ]
 df_power.to_csv("data_storage/power.csv", index=False)
@@ -80,9 +77,9 @@ df_gas = df[
         "date_eom",
         "date",
         "days",
-        "gaz_m3",
-        "cons_gaz",
-        "av_day",
+        "gas_m3",
+        "g_cons",
+        "g_av_day",
     ]
 ]
 df_gas.to_csv("data_storage/gas.csv", index=False)
