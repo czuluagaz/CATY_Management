b248d008a44dd9f58321ff07d0e62c51d9443903
czuluagaz <139249956+czuluagaz@users.noreply.github.com>
2024-06-21 17:50:00 +0200

    First Commit

---
+++ core/forecast.py
@@ -0,0 +1,19 @@
+# Forecast 
+# Path:
+# Date:
+# Version:
+
+"""
+This script has the main method use for forecast the data using different models
+
+"""
+
+# import the necessary libraries
+# Import the necessary libraries
+import sys
+from os import path
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+# Variables
